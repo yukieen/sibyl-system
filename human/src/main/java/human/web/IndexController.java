1df5279dd471996444ec3109fe247899d623c108
yukieen <lpczclt@gmail.com>
2014-12-27 16:12:20 +0900

    human service を追加.metrics にpsycho-passを追加

---
+++ human/src/main/java/human/web/IndexController.java
@@ -0,0 +1,15 @@
+package human.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by haljik on 14/12/27.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "hoge";
+    }
+}
