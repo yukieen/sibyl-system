a8cee5c8bee5d6e137f2adfe8675897ad2042204
yukieen <lpczclt@gmail.com>
2014-12-28 02:23:35 +0900

    paralize メソッドをPOSTに変更

--- human/src/main/java/human/web/HogeController.java
+++ human/src/main/java/human/web/HogeController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -44,7 +45,7 @@ public class HogeController {
         return "hoge";
     }
     
-    @RequestMapping("/paralize")
+    @RequestMapping(value = "/paralize", method = RequestMethod.POST)
     public String paralize(@ModelAttribute Human human){
     	human.paralize();
     	return "hoge";
